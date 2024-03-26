f4ce8f23023a91d2d5e1356fa5f7875f3331542a
João Luciano <104032132+JoaoLuciano1429@users.noreply.github.com>
2024-03-26 19:54:28 -0300

    Formulário de Contato

--- js/script.js
+++ js/script.js
@@ -13,3 +13,25 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     }
   });
 });
+
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let formData = new FormData(this);
+
+    fetch(this.action, {
+      method: "POST",
+      body: formData,
+    })
+      .then((response) => {
+        if (response.ok) {
+          alert("Mensagem enviada com sucesso!");
+          this.reset(); // Reseta o formulário
+        } else {
+          alert("Ocorreu um erro ao enviar a mensagem.");
+        }
+      })
+      .catch((error) => console.error("Erro ao enviar o formulário:", error));
+  });
